178087f7bb22d577c69ba3830671708070c11857
Tiago Filipe Alberto Antunes <tialbert@c1r4s7.42porto.com>
2025-04-24 12:29:10 +0100

    fix: Removed commented lines

--- ex01/Include/ScavTrap.hpp
+++ ex01/Include/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: tialbert <tialbert@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 16:06:00 by tialbert          #+#    #+#             */
-/*   Updated: 2025/03/23 18:13:53 by tialbert         ###   ########.fr       */
+/*   Updated: 2025/04/24 11:43:15 by tialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,6 @@ class ScavTrap: public ClapTrap {
 		ScavTrap( void );
 		ScavTrap( std::string name );
 		ScavTrap( ScavTrap const &st );
-	// : ClapTrap(st) {
-	// 		*this = st;
-	// 		std::cout << "ScavTrap " << _name << ": Copy constructor called" << " (Total health: " << _hitPoints << "; Total energy: " << _energyPoints << ")" << std::endl;
-	// 	};
 		ScavTrap &operator= ( ScavTrap const &st );
 		~ScavTrap( void );
 		void	attack( std::string target );
